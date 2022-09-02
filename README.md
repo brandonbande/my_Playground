d90f69bd08d454453aa0995ad6f718046101918d
brandonbande <89384636+brandonbande@users.noreply.github.com>
2022-09-02 08:16:48 +0200

    Data Received...Lets clean it
    Working with open Weather map API...beginner level

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # my_Playground
  various codes for web and all
+
+ #General Concept
+ Supposed to take ia all the Tourist Resorts in Zimbabwe (big/Small)
+
+ For Each Resort
+ -Get LatLong (Direction Purposes)
+ -Weather Data (4 Day Predictions)
+ -Classify by activities & Location
+ -News from Social Media mining
+
+ #Extras
+ -Close By Accomodation , Courrier Services , Tour Guides , etc
+
+ # TouristFunctionality
+  -Recommender System
+  -Distance Calculator
+  - Cost Estimater
+  -Booking Facilitys
+
+# Stats , Tweets
--- openweather.js
+++ openweather.js
@@ -1,8 +1,10 @@
-const api_key = '6f8152d7b99325ffe2be596042a07be4';
-const my_lat = -19.45;
-const my_lon = 29.8167;
-//myurl2 = 'http://api.openweathermap.org/data/2.5/weather?q=Gweru,zw&APPID=6f8152d7b99325ffe2be596042a07be4';
-const url= 'api.openweathermap.org/data/2.5/forecast?lat={'+my_lat+'}&lon={'+my_lon+'}&appid={'+api_key+'}';
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Gweru,zw&APPID=6f8152d7b99325ffe2be596042a07be4')
-    .then(res => console.log(res))
-    .then(res=>res)
+const api_key = '6f8152d7b99325ffe2be596042a07be4'; //My API_Key from openweathermap
+const my_lat = -19.45;//Lattitude
+const my_lon = 29.8167; //Longitude
+myurl2 = 'http://api.openweathermap.org/data/2.5/weather?q=Gweru,zw&APPID='+api_key; //access current weather updates
+const url= 'http://api.openweathermap.org/data/2.5/forecast?lat='+my_lat+'&lon='+my_lon+'&APPID='+api_key; //access 4 hour forecasts
+fetch(url)
+    .then(res => res.json()) //get a response and convert to json object
+    .then(data=> console.log(data)) //data then displayed
+.catch(err => console.log(err)) //catch error
+
